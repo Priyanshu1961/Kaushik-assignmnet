b2d86170cf0c330b7a3adda573d4d2340b3e0111
kaushik <kaushik.t@reportgarden.com>
2017-07-04 20:50:12 +0530

    indentation

--- Ruby/as2_ruby.rb
+++ Ruby/as2_ruby.rb
@@ -38,7 +38,7 @@ def calculate_avg
     raise EmptyStringError if size == 0
     rescue EmptyStringError => e
     puts e.class
-    puts e.msg# "my thing"
+    puts e.msg
     return
   end
   #loop to replace all the non integers in the string with " "
@@ -47,30 +47,30 @@ def calculate_avg
   values = inp.split(" ")
   #check for integers
   begin
-    raise NoIntegerError if values.count==0
+    raise NoIntegerError if values.count == 0
     rescue NoIntegerError => e
     puts e.class
-    puts e.msg# "my thing"
+    puts e.msg
     return
   end
   sum = 0
   count = 0
   values.each do |i|
-    if (i.to_i>0)
+    if (i.to_i > 0)
       sum += i.to_i
-      count +=1
+      count += 1
     end
   end
   #check for positive integers
   begin
-    raise NoPositiveIntegerError if count==0
+    raise NoPositiveIntegerError if count == 0
     rescue NoPositiveIntegerError => e
     puts e.class
-    puts e.msg# "my thing"
+    puts e.msg
     return
   end
   puts "number of positive integers in the given string are #{count}"
-  puts "The average of those positive integers  is #{sum.to_f/count}"
+  puts "The average of those positive integers  is #{sum.to_f / count}"
 end
 #Function call
 calculate_avg
